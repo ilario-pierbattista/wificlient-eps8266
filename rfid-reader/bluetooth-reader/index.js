4997c23ca4b5004f16d224288d58cee0500a964b
vinsds <vinsds@gmail.com>
2016-05-02 17:03:06 +0200

    BT-Lettura Seriale Node.js

---
+++ rfid-reader/bluetooth-reader/index.js
@@ -0,0 +1,39 @@
+var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+var exit = require('exit');
+
+const BT_ADDRESS = "20-13-07-19-14-98";
+
+var global_buffer = "";
+
+btSerial.on('found', function(address,name){
+  console.log("Nome: "+name+" - "+"Indirizzo: "+address);
+
+  if(address == BT_ADDRESS) {
+    console.log("Device found! \n");
+
+    btSerial.findSerialPortChannel(address, function(channel) {
+      console.log("Serial channel: "+channel);
+
+      btSerial.connect(address, channel, function(){
+        console.log("Connected!");
+
+        btSerial.on('data', function(buffer) {
+          var piece = buffer.toString("utf-8");
+          global_buffer += piece.replace(/(\r\n|\n\r|\n|\r)/gm,"");
+          if(piece.indexOf("\n") > -1 && piece.indexOf("\r") > -1) {
+            console.log(global_buffer);
+
+            // @TODO inviare la richiesta al server
+
+            global_buffer = "";
+          }
+        });
+
+      }, function() {
+        console.log("Cannot connect!");
+        exit(1);
+      });
+    });
+  }
+});
+btSerial.inquire();
