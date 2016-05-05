2465eba42d91129bffcb949796c9f8828254cd0d
vinsds <vinsds@gmail.com>
2016-05-05 10:10:49 +0200

    Invio lettura RFID al server.

--- rfid-reader/bluetooth-reader/index.js
+++ rfid-reader/bluetooth-reader/index.js
@@ -1,19 +1,23 @@
 var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 var exit = require('exit');
+var rest = require('restler');
+var colors = require('colors');
 
-const BT_ADDRESS = "20-13-07-19-14-98";
+
+const BT_ADDRESS = '20-13-07-19-14-98';
+const HOST = 'http://192.168.0.2';
+const API_GET_REQUEST = '/camerino/rfid';
 
 var global_buffer = "";
 
 btSerial.on('found', function(address,name){
-  console.log("Nome: "+name+" - "+"Indirizzo: "+address);
+  //console.log("Nome: "+name+" - "+"Indirizzo: "+address);
 
   if(address == BT_ADDRESS) {
-    console.log("Device found! \n");
+    console.log("Dispositivo trovato!");
+    console.log("Connessione in corso...");
 
     btSerial.findSerialPortChannel(address, function(channel) {
-      console.log("Serial channel: "+channel);
-
       btSerial.connect(address, channel, function(){
         console.log("Connected!");
 
@@ -21,10 +25,22 @@ btSerial.on('found', function(address,name){
           var piece = buffer.toString("utf-8");
           global_buffer += piece.replace(/(\r\n|\n\r|\n|\r)/gm,"");
           if(piece.indexOf(";") > -1) {
-            console.log(global_buffer);
+            console.log(global_buffer.bold);
 
             //@TODO inviare la richiesta al server
-
+            rest.get(HOST+API_GET_REQUEST).on('complete', function(result) {
+              if (result instanceof Error) {
+                console.log('Error:', result.message);
+                this.retry(5000);
+              } else {
+                //var json = JSON.parse(result);
+                if (result['true']) {
+                  console.log("TRUE".bgGreen);
+                } else{
+                  console.log("FALSE".bgRed);
+                }
+              }
+            });
             global_buffer = "";
           }
         });
